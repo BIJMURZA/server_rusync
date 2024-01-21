23559615fb0c9afee0feb95388a2fa6bfab6a2f2
KUROSAKI <xxxbijumrzaxxx@gmail.com>
2024-01-21 21:11:47 +0700

    Вывод price таблиц

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,22 @@ const express = require('express');
 const router = express.Router();
 const pool = require('./postgres_rusync')
 
-router.get('/data/:tableName', async (req, res) => {
-  try {
-    const tableName = req.params.tableName;
-    const query = `SELECT * FROM "${tableName}"`;
-    const { rows } = await pool.query(query);
-    const gameNames = rows.map(row => row.game_name);
-    console.log(gameNames[1]);
-    console.log(rows)
-    res.json(rows);
-  } catch (err) {
-    console.error("Ошибка запроса к базе данных:", err);
-    res.status(500).send(err.message);
+router.get('/data/:price', async (req, res) => {
+  let whiteMarketplaces = ['steam', 'steampay', 'steambuy', 'gamerz', 'game_mag', 'zaka_zaka', 'gabestore'];
+  const allData = [];
+
+  for (const marketplace of whiteMarketplaces) {
+    try {
+      const query = `SELECT game_name, price FROM ${marketplace};`;
+      const {rows} = await pool.query(query);
+      allData.push(rows);
+      console.log(marketplace)
+    } catch (err) {
+      console.error("Ошибка запроса к базе данных:", err);
+      res.status(500).send(err.message);
+    }
   }
+  res.json(allData);
 });
 
 module.exports = router;
