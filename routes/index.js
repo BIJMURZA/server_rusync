4a895c557effc2a48065a434a9995e5767fc8341
KUROSAKI <xxxbijumrzaxxx@gmail.com>
2024-01-21 20:45:07 +0700

    Получение обращения от React и вывод необходимой информации из таблицы

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,19 @@ const express = require('express');
 const router = express.Router();
 const pool = require('./postgres_rusync')
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-router.get('/data', async(req, res) => {
+router.get('/data/:tableName', async (req, res) => {
   try {
-    const tabs  = await pool.query('SELECT * FROM steam;');
-    console.log('Запрос успешен!', tabs.rows);
-    res.json( tabs.rows )
+    const tableName = req.params.tableName;
+    const query = `SELECT * FROM "${tableName}"`;
+    const { rows } = await pool.query(query);
+    const gameNames = rows.map(row => row.game_name);
+    console.log(gameNames[1]);
+    console.log(rows)
+    res.json(rows);
   } catch (err) {
+    console.error("Ошибка запроса к базе данных:", err);
     res.status(500).send(err.message);
-    console.log("Запрос неуспешен!", err);
   }
-})
+});
 
 module.exports = router;
